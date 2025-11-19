4490836bd51314368d66e76bd4afd3018bbb89d8
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-11-19 18:57:08 +0900

    fix format

--- example/m5atom-car/gen-index-html.bat
+++ example/m5atom-car/gen-index-html.bat
@@ -4,6 +4,6 @@ REM Brotli圧縮されたHTMLをダウンロードし、C++バイト配列に変
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0gen-index-html.ps1"
 
 if %errorlevel% neq 0 (
-        echo Error occurred during generation
+    echo Error occurred during generation
     exit /b %errorlevel%
 )
